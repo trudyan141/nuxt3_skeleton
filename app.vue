2de65cf185c51b7a0e8c7c4f6c2ba34a0debc3a5
trunglk-ibl <trunglk@blockchainlabs.asia>
2024-02-26 11:24:30 +0700

    update nuxt3 sleleton

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import { theme } from 'ant-design-vue';
+</script>
+<template>
+  <div>
+  <a-config-provider
+     :theme="{
+      algorithm: theme.defaultAlgorithm,
+    }"
+  >
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
+   </a-config-provider>
+   
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,37 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+   css: ["@/assets/css/main.scss"],
+   vite: {
+     plugins: [
+      Components({
+        resolvers: [AntDesignVueResolver({
+          importStyle: 'less',
+        })],
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/css/variable.scss" as *;',
+        },
+      },
+    },
+    ssr: {
+      noExternal: ['moment', 'compute-scroll-into-view', 'ant-design-vue'],
+    },
+  },
+  modules: [
+    "@nuxt/image", 
+    'nuxt3-vuex-module',
+    '@vee-validate/nuxt'
+  ],
+  plugins: [
+    '@/plugins/antd',
+    '@/plugins/mixin-global',
+    '@/plugins/vue-i18n',
+    '@/plugins/vee-validate-rules'
+  ]
+})
